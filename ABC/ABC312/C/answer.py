b4d044f66e6f94014bf8cdca4c77ea720667c9fb
nzmort <118791096+nozomuorita@users.noreply.github.com>
2023-07-30 17:53:11 +0900

    AC

--- ABC/ABC312/C/answer.py
+++ ABC/ABC312/C/answer.py
@@ -1,152 +1,31 @@
-# from collections import defaultdict
-# import bisect
-# n, m = map(int, input().split())
-# a = list(map(int, input().split()))
-# b = list(map(int, input().split()))
+from bisect import bisect_left, bisect_right
 
-# b.sort(reverse=True)
-# a.sort()
-
-# db = defaultdict(int)
-# da = defaultdict(int)
-
-# na = 0
-# for i in range(n):
-#     da[a[i]] = na + 1
-#     na += 1
-
-# nb = 0
-# for i in range(m):
-#     db[b[i]] = nb + 1
-#     nb += 1
-
-# db_keys = list(db.keys())
-# db_keys.sort()
-# da_keys = list(da.keys())
-# da_keys.sort()
-# # print(db_keys)
-# # print(da)
-# # print(db)
-
-# for key in list(da.keys()):
-#     #print(key)
-#     # for key in [keys, keys+1]:
-#     num = bisect.bisect_left(db_keys, key)
-#     if num >= len(db_keys):
-#         continue
-#     # print('num', num)
-#     # print(db_keys[num])
-#     if da[key] >= db[db_keys[num]]:
-#         print(key)
-#         exit()
-
-# print(max(db_keys)+1)
-
-# from collections import defaultdict
-# import bisect
-# n, m = map(int, input().split())
-# a = list(map(int, input().split()))
-# b = list(map(int, input().split()))
-
-# b.sort(reverse=True)
-# a.sort()
-
-# db = defaultdict(int)
-# da = defaultdict(int)
-
-# na = 0
-# for i in range(n):
-#     da[a[i]] = na + 1
-#     da[a[i]+1] = na + 1
-#     na += 1
-
-# nb = 0
-# for i in range(m):
-#     db[b[i]] = nb + 1
-#     db[b[i]+1] = nb
-#     nb += 1
-
-# db_keys = list(db.keys())
-# db_keys.sort()
-# da_keys = list(da.keys())
-# da_keys.sort()
-# # print(db_keys)
-# # print(da)
-# # print(db)
-# c = a.copy()
-# c_sub = [i+1 for i in b]
-# c.extend(c_sub)
-# c.sort()
-
-# # for keys in list(da.keys()):
-# for keys in c:
-#     #print(key)
-#     for key in [keys, keys+1]:
-#         num = bisect.bisect_left(db_keys, key)
-#         if num >= len(db_keys):
-#             continue
-#         # print('num', num)
-#         # print(db_keys[num])
-#         if da[key] >= db[db_keys[num]]:
-#             print(key)
-#             exit()
-
-# print(max(b)+1)
-
-from collections import defaultdict
-import bisect
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-b.sort(reverse=True)
+# 二分探索をするためソートする必要がある
 a.sort()
+b.sort()
 
-db = defaultdict(int)
-da = defaultdict(int)
-
-na = 0
-for i in range(n):
-    da[a[i]] = na + 1
-    da[a[i]+1] = na + 1
-    na += 1
-
-nb = 0
-for i in range(m):
-    db[b[i]] = nb + 1
-    db[b[i]+1] = nb
-    nb += 1
-
-db_keys = list(db.keys())
-db_keys.sort()
-da_keys = list(da.keys())
-da_keys.sort()
-# print(db_keys)
-# print(da)
-# print(db)
+# ansとなりうるのは、A_i か (B_i)+1 である
+# そのためそれらの値をまとめたリストを作る
 c = a.copy()
-c_sub = [i+1 for i in b]
-c.extend(c_sub)
+for i in b:
+    c.append(i+1)
+c = list(set(c)) # 一度set型にして重複をなくす
 c.sort()
 
-# for keys in list(da.keys()):
-for keys in c:
-    #print(key)
-    if da[keys] >= db[keys]:
-        print(keys)
-        exit()
-        
-
+# 答えになりうる値について一つずつ確認
+for i in c:
+    # i円で売りたい人数は、右側挿入時のインデックス
+    num_n = bisect_right(a, i)
+    # i円で買いたい人は、左側挿入時インデックスをmから引いたもの
+    num_m = m - bisect_left(b, i)
     
-    
-    # for key in [keys, keys+1]:
-    #     num = bisect.bisect_left(db_keys, key)
-    #     if num >= len(db_keys):
-    #         continue
-    #     # print('num', num)
-    #     # print(db_keys[num])
-    #     if da[key] >= db[db_keys[num]]:
-    #         print(key)
-    #         exit()
+    # 売りたい人数のほうが大きいなら答え
+    if num_n >= num_m:
+        ans = i
+        break
 
-print(max(b)+1)
+print(ans)
