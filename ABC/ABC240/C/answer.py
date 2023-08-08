e38b9470368630118d4a879bf3eb331ff298e991
nzmort <118791096+nozomuorita@users.noreply.github.com>
2023-08-09 03:16:17 +0900

    first commit

--- ABC/ABC240/C/answer.py
+++ ABC/ABC240/C/answer.py
@@ -1,3 +1,20 @@
+# @nzm_ort
+# https://github.com/nozomuorita/atcoder-workspace-python
+
+# import module ------------------------------------------------------------------------------
+from collections import defaultdict, deque, Counter
+import math
+from itertools import combinations, permutations, product, accumulate, groupby, chain
+from heapq import heapify, heappop, heappush
+import bisect
+import sys
+# sys.setrecursionlimit(100000000)
+inf = float('inf')
+mod1 = 10**9+7
+mod2 = 998244353
+def ceil_div(x, y): return -(-x//y) # 切り上げ
+
+# main code ------------------------------------------------------------------------------------
 # 解説AC
 n, x = map(int, input().split())
 ab = [list(map(int, input().split())) for _ in range(n)]
