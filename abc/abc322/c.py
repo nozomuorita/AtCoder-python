9592eb3be5fc33908512705ffdf9cab4cbe1a7dc
nzmort <118791096+nozomuorita@users.noreply.github.com>
2023-10-03 07:49:20 +0900

    update

--- abc/abc322/c.py
+++ abc/abc322/c.py
@@ -3,10 +3,6 @@ from bisect import bisect_left, bisect_right, insort_left, insort_right
 
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
-
-# d = defaultdict(int)
-# for i in range(m):
-#     d[a[i]] += 1
     
 for i in range(1, n+1):
     idx = bisect_left(a, i)
