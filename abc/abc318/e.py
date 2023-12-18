58a966e70476e09029f1b74a83cb6029b7cfa0e0
nzmort <118791096+nozomuorita@users.noreply.github.com>
2023-12-18 21:24:16 +0900

    'hoge'

--- abc/abc318/e.py
+++ abc/abc318/e.py
@@ -1,54 +1,34 @@
-import sys
 from collections import defaultdict
-sys.setrecursionlimit(100000000)
-
 n = int(input())
-visited = [False] * n
-ans = -1
+a = list(map(int, input().split()))
 
-# g = []
-# for i in range(n-1):
-#     tmp = list(map(int, input().split()))
-#     g.append(tmp)
-g = defaultdict(int)
-for i in range(n-1):
-    tmp = list(map(int, input().split()))
-    tmp_n = i+1
-    for j in tmp:
-        g[(i, tmp_n)] = j
-        tmp_n += 1
-print(g)
-    
-def dfs(i, cost):
-    global ans
-    global visited
-    visited[i] = True
-    if visited.count(True)==n:
-        if cost>ans:
-            ans = cost
-            print(f'aaa: {cost}{i}')
-    
-    for j in range(n):
-        if (visited[j]) or (i==j):
-            continue
-        print(i, j)
-        #cost += g[min(i, j)][max(i, j)]
-        cost += g[(min(i, j), max(i, j))]
-        visited[j] = True
-        if visited.count(True)==n:
-            if cost>ans:
-                ans = cost
-                print(f'aaa: {cost}, {i}')
-        else:
-            for k in range(n):
-                if (visited[k]):
-                    continue
-                else:
-                    dfs(k, cost)
-                    visited[j] = False
-    visited[i] = False
-        
-    #visited[k] = False
+dl, dr = defaultdict(int), defaultdict(int)
+dl[a[0]] += 1
+for i in range(1, n): dr[a[i]]+=1
 
-dfs(0, 0)
+s = 0
+for key in list(dl.keys()):
+    s += dl[key]*dr[key]
+
+ans = 0
+for i in range(1, n-1):
+    ans += s - dl[a[i]]*dr[a[i]]
+    
+    s -= dl[a[i]]*dr[a[i]]
+    dl[a[i]] += 1
+    dr[a[i]] -= 1
+    s += dl[a[i]]*dr[a[i]]
+
+    #print(dl)
+    #print(0, dr)
+    #print(ans, s)
+    #ans += s - dl[a[i]]*dr[a[i]]
+
+    #s -= dl[a[i]]*dr[a[i]]
+    #s -= dl[a[i+1]]*dr[a[i+1]]
+    #dl[a[i]] += 1
+    #dr[a[i+1]] -= 1
+    #s += dl[a[i]]*dr[a[i]]
+    #s += dl[a[i+1]]*dr[a[i+1]]
+
 print(ans)
